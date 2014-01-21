27c40b1406b8170431e9f9a5bd21e22e631a16f0
wujysh <wujysh@gmail.com>
2014-01-21 10:42:49 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DempApp::Application.config.secret_key_base = 'ecbf284159ee236642a251a59eeb4089623224a5af528a22ab09760b6677f329d001c75a9c7cc798c02fc73f66313721e68448e1fa3652ab5d953e23d51b0c8b'
